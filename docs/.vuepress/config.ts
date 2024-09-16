32283d0e1a582b38196cbb6cba9ee978a2cb4a0b
KatePril <ket14pril14@ukr.net>
2024-09-16 16:56:15 +0300

    Initial commit

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,11 +4,11 @@ import MarkdownItPlantuml from 'markdown-it-plantuml';
 
 export default defineUserConfig({
   // Замінити на назву свого репозиторія
-  base: "/db-course-hope-template/",
+  base: "/db_labs_demo/",
 
   lang: "en-US",
   // Замінити на назву свого проєкту
-  title: "Назва проєкту",
+  title: "Test",
   description: "Лабораторні роботи",
 
   theme,
